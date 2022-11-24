3f65f42f10a4c452bbb4a6deba1d0af6fdc6364d
Progger715 <83240866+Progger715@users.noreply.github.com>
2022-11-24 17:56:58 +0300

    all protocols correct work

---
+++ client/udp_client.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+host = 'localhost'
+port = 8080
+addr = (host, port)
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+data = input('write to server: ')
+if not data:
+    udp_socket.close()
+    sys.exit(1)
+
+# encode - перекодирует введенные данные в байты, decode - обратно
+data = str.encode(data)
+udp_socket.sendto(data, addr)
+data = bytes.decode(data)
+data = udp_socket.recvfrom(1024)
+print(data)
+
+udp_socket.close()
